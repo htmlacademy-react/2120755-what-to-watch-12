f44ba9560b14cbdf971d5a4902317984c0e46624
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-06-02 15:25:02 +0300

    test for reducres done

---
+++ project/src/store/reducers/authorization.test.ts
@@ -0,0 +1,33 @@
+import { authorizationSlice } from './authorization';
+import { checkAuthAction, login, logout } from '../api-actions';
+import { mockUserData } from '../../mocks/mock-user-data';
+
+const userData = mockUserData;
+const state = { authorized: false, userData: undefined};
+
+describe('Reducer: authorization', () => {
+  it('without additional parameters should return initial state', () => {
+    expect(authorizationSlice.reducer(void 0, {type: 'UNKNOWN_ACTION'}))
+      .toEqual({ authorized: false, userData: undefined});
+  });
+  it('should update authorization state to "true" and update userData state with loaded data if response recived from checkAuthAction', () => {
+    expect(authorizationSlice.reducer(state, {type: checkAuthAction.fulfilled.type, payload: userData}))
+      .toEqual({ authorized: true, userData: userData});
+  });
+  it('should update authorization state to "false" if response recived with rejeсtion from checkAuthAction', () => {
+    expect(authorizationSlice.reducer(state, {type: checkAuthAction.rejected.type}))
+      .toEqual({ authorized: false, userData: undefined});
+  });
+  it('should update authorization state to "true" and update userData state with loaded data if response recived from login', () => {
+    expect(authorizationSlice.reducer(state, {type: login.fulfilled.type, payload: userData}))
+      .toEqual({ authorized: true, userData: userData});
+  });
+  it('should update authorization state to "false" if response recived with rejeсtion from login', () => {
+    expect(authorizationSlice.reducer(state, {type: login.rejected.type}))
+      .toEqual({ authorized: false, userData: undefined});
+  });
+  it('should update authorization state to "false" and userData to "underfined" if response recived from logout', () => {
+    expect(authorizationSlice.reducer({ authorized: false, userData: mockUserData}, {type: logout.fulfilled.type}))
+      .toEqual({ authorized: false, userData: undefined});
+  });
+});
---
+++ project/src/store/reducers/loading.test.ts
@@ -0,0 +1,60 @@
+import { cleanFavoriteFilmsLoadingStatus, cleanFilmLoadingStatus, loadingSlice } from './loading';
+import { fetchFilms, fetchFilmData, fetchUserFilms } from '../api-actions';
+
+
+const state = {
+  isLoaded: false,
+  isFilmLoaded: false,
+  isFavoriteFilmsLoaded: false};
+
+describe('Reducer: loading', () => {
+  it('without additional parameters should return initial state', () => {
+    expect(loadingSlice.reducer(void 0, {type: 'UNKNOWN_ACTION'}))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: false});
+  });
+  it('should update isLoaded then load completed', () => {
+    expect(loadingSlice.reducer(state, {type: fetchFilms.fulfilled.type}))
+      .toEqual({
+        isLoaded: true,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: false});
+  });
+  it('should update isOfferLoaded then load completed', () => {
+    expect(loadingSlice.reducer(state, {type: fetchFilmData.fulfilled.type}))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: true,
+        isFavoriteFilmsLoaded: false});
+  });
+  it('should update  isFavoriteFilmsLoaded then load completed', () => {
+    expect(loadingSlice.reducer(state, {type: fetchUserFilms.fulfilled.type}))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: true});
+  });
+  it('should update isOfferLoaded then load in progress', () => {
+    expect(loadingSlice.reducer(state, {type: fetchFilmData.rejected.type}))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: false});
+  });
+  it('should update isFilmLoaded to false', () => {
+    expect(loadingSlice.reducer(state, {type: cleanFilmLoadingStatus }))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: false});
+  });
+  it('should update isFavoriteFilmsLoaded to false', () => {
+    expect(loadingSlice.reducer(state, {type: cleanFavoriteFilmsLoadingStatus }))
+      .toEqual({
+        isLoaded: false,
+        isFilmLoaded: false,
+        isFavoriteFilmsLoaded: false});
+  });
+});
